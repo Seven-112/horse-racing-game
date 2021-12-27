4bf6d843a463224b68136491bd13839046bd3db7
Seven-112 <greensatellite777@gmail.com>
2021-12-27 09:55:02 +0800

    update

--- programs/horse-racing/src/lib.rs
+++ programs/horse-racing/src/lib.rs
@@ -9,8 +9,10 @@ use solana_program::{
 declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 
 pub const UPGRADABLE_METASIZE: usize = 1 + 1 + 1;
-pub const NFT_LIST_SIZE: usize = 32 * 1000;
+pub const NFT_LIST_SIZE: usize = 2 + 32 * 1000; // 2 bytes for nft count
+pub const OPERATOR_LIST_SIZE: usize = 32 * 10 + 2;
 pub const MAX_ADMIN_CNT: usize = 10;
+pub const RACE_RESULT_SIZE: usize = 32;
 
 pub const BTC_DECIMALS: usize = 9;
 pub const SOL_DECIMALS: usize = 9;
@@ -23,7 +25,8 @@ pub mod horse_racing {
     pub fn initialize(
         ctx: Context<Initialize>,
         nft_list_bump: u8,
-        operator_list_bump: u8
+        operator_list_bump: u8,
+        race_bump: u8
     ) -> ProgramResult {
 
         let mut operator_list = &mut ctx.accounts.operator_list;
@@ -43,34 +46,36 @@ pub mod horse_racing {
 
     pub fn mint_nft(
         ctx: Context<MintNFT>,
-        _bump: u8
+        metadata_bump: u8,
+        nft_list_bump: u8
     ) -> ProgramResult {
-        let clock = Clock::from_account_info(&ctx.accounts.clock)?;
 
         let upgradable_metadata = &mut ctx.accounts.upgradable_metadata;
-        upgradable_metadata.bump = _bump;
+        upgradable_metadata.bump = metadata_bump;
         upgradable_metadata.passion = 0;
         upgradable_metadata.stamina = 0;
 
         let sol_price = chainlink::get_price(&chainlink::id(), ctx.accounts.sol_feed_account)?;
         let btc_price = chainlink::get_price(&chainlink::id(), ctx.accounts.btc_feed_account)?;
         if let Some(sol_price) = sol_price {
-            let rand_from_sol = sol_price as u64 + clock.unix_timestamp as u64;
+            let rand_from_sol = sol_price as u64 + ctx.accounts.clock.unix_timestamp as u64;
             upgradable_metadata.passion = (rand_from_sol % 10) as u8 + MIN_PASSION;
             msg!("Sol Price is {}", sol_price);
         } else {
-            upgradable_metadata.passion = (clock.unix_timestamp % 10) as u8 + MIN_PASSION;
+            upgradable_metadata.passion = (ctx.accounts.clock.unix_timestamp % 10) as u8 + MIN_PASSION;
             msg!("No current Sol price");
         }
 
         if let Some(btc_price) = btc_price {
-            let rand_from_sol = btc_price as u64 + clock.unix_timestamp as u64;
+            let rand_from_sol = btc_price as u64 + ctx.accounts.clock.unix_timestamp as u64;
             upgradable_metadata.stamina = (rand_from_sol % 10) as u8 + MIN_STAMINA;
             msg!("BTC Price is {}", btc_price);
         } else {
-            upgradable_metadata.stamina = (clock.unix_timestamp % 10) as u8 + MIN_STAMINA;
+            upgradable_metadata.stamina = (ctx.accounts.clock.unix_timestamp % 10) as u8 + MIN_STAMINA;
             msg!("No current BTC price");
         }
+
+        add_nft(ctx.accounts.mint.key(), ctx.accounts.nft_list)?;
 
         Ok(())
     }
@@ -78,35 +83,82 @@ pub mod horse_racing {
     pub fn upgrade_nft(
         ctx: Context<UpgradeNFT>
     ) -> ProgramResult {
-        let clock = Clock::from_account_info(&ctx.accounts.clock)?;
+        
         let upgradable_metadata = &mut ctx.accounts.upgradable_metadata;
 
         let sol_price = chainlink::get_price(&chainlink::id(), ctx.accounts.sol_feed_account)?;
         let btc_price = chainlink::get_price(&chainlink::id(), ctx.accounts.btc_feed_account)?;
         if let Some(sol_price) = sol_price {
-            let rand_from_sol = sol_price as u64 + clock.unix_timestamp as u64;
+            let rand_from_sol = sol_price as u64 + ctx.accounts.clock.unix_timestamp as u64;
             upgradable_metadata.passion = (rand_from_sol % 10) as u8 + upgradable_metadata.passion;
             msg!("Sol Price is {}", sol_price);
         } else {
-            upgradable_metadata.passion = (clock.unix_timestamp % 10) as u8 + upgradable_metadata.passion;
+            upgradable_metadata.passion = (ctx.accounts.clock.unix_timestamp % 10) as u8 + upgradable_metadata.passion;
             msg!("No current Sol price");
         }
 
         if let Some(btc_price) = btc_price {
-            let rand_from_sol = btc_price as u64 + clock.unix_timestamp as u64;
+            let rand_from_sol = btc_price as u64 + ctx.accounts.clock.unix_timestamp as u64;
             upgradable_metadata.stamina = (rand_from_sol % 10) as u8 + upgradable_metadata.stamina;
             msg!("BTC Price is {}", btc_price);
         } else {
-            upgradable_metadata.stamina = (clock.unix_timestamp % 10) as u8 + upgradable_metadata.stamina;
+            upgradable_metadata.stamina = (ctx.accounts.clock.unix_timestamp % 10) as u8 + upgradable_metadata.stamina;
             msg!("No current BTC price");
         }
 
         Ok(())
     }
+
+    pub fn start_race(
+        ctx: Context<StartRace>
+    ) -> ProgramResult {
+
+        let operator_list = &mut ctx.accounts.operator_list;
+        let flag: u8 = 0;
+        for item in operator_list.operator_array.iter().enumerate() {
+            let (i, op): (usize, &Pubkey) = item;
+            if *op == ctx.accounts.operator.key() {
+                flag = 1;
+                break;
+            }
+        }
+
+        if flag != 1 {
+            msg!("You are not operator to start race");
+            return Err(ProgramError::InvalidAccountData);
+        }
+
+        let sol_price = chainlink::get_price(&chainlink::id(), ctx.accounts.sol_feed_account)?;
+        let btc_price = chainlink::get_price(&chainlink::id(), ctx.accounts.btc_feed_account)?;
+        let random_value: u64 = 0;
+        if let Some(sol_price) = sol_price {
+            random_value += sol_price;
+            msg!("Sol Price is {}", sol_price);
+        } else {
+            msg!("No current Sol price");
+        }
+
+        if let Some(btc_price) = btc_price {
+            random_value += btc_price;
+            msg!("BTC Price is {}", btc_price);
+        } else {
+            msg!("No current BTC price");
+        }
+        random_value += ctx.accounts.clock.unix_timestamp as u64;
+
+        let nft_count = get_nft_count(ctx.accounts.nft_list)?;
+        let winner_idx = random_value % nft_count as u64;
+        msg!("winner is {} th horse!", winner_idx);
+
+        // getting Pubkey of winner
+        let winner_pk = get_winner(winner_idx, ctx.accounts.nft_list)?;
+        ctx.accounts.race_result.winner = winner_pk;
+        Ok(())
+    }
 }
 
 #[derive(Accounts)]
-#[instruction(nft_list_bump: u8, operator_list_bump: u8)]
+#[instruction(nft_list_bump: u8, operator_list_bump: u8, race_bump: u8)]
 pub struct Initialize<'info> {
     #[account(mut)]
     pub admin: Signer<'info>,
@@ -114,7 +166,6 @@ pub struct Initialize<'info> {
     #[account(
         mut,
         seeds = [
-            (*admin.key).as_ref(), 
             (*program_id).as_ref(), 
             "nft_list".as_ref()
         ],
@@ -125,14 +176,26 @@ pub struct Initialize<'info> {
     #[account(
         init,
         seeds = [
-            (*admin.key).as_ref(),
             (*program_id).as_ref(), 
-            "admin_list".as_ref()
+            "operator_list".as_ref()
         ],
         bump = operator_list_bump,
-        payer = admin
+        payer = admin,
+        space = 8 + OPERATOR_LIST_SIZE
     )]
-    pub operator_list: Account<'info, OperatorWhiteList>,
+    pub operator_list: ProgramAccount<'info, OperatorWhiteList>,
+
+    #[account(
+        init,
+        seeds = [
+            (*program_id).as_ref(),
+            "race_result".as_ref()
+        ],
+        bump = race_bump,
+        payer = admin,
+        space = 8 + RACE_RESULT_SIZE
+    )]
+    pub race_result: ProgramAccount<'info, RaceResult>,
 
     pub system_program: Program<'info, System>,
 }
@@ -149,7 +212,7 @@ pub struct AddOperator<'info> {
 }
 
 #[derive(Accounts)]
-#[instruction(_bump : u8)]
+#[instruction(metadata_bump : u8, nft_list_bump: u8)]
 pub struct MintNFT<'info> {
     #[account(mut, signer)]
     pub admin: AccountInfo<'info>,
@@ -158,13 +221,23 @@ pub struct MintNFT<'info> {
     pub owner: Signer<'info>,
 
     #[account(
+        mut,
+        seeds = [
+            (*program_id).as_ref(), 
+            "nft_list".as_ref()
+        ],
+        bump = nft_list_bump
+    )]
+    pub nft_list: AccountInfo<'info>,    
+
+    #[account(
         init,
         seeds = [(*owner.key).as_ref(), (*mint.key).as_ref()],
-        bump = _bump,
+        bump = metadata_bump,
         payer = owner,
         space = 8 + UPGRADABLE_METASIZE
     )]
-    pub upgradable_metadata: Account<'info, UpgradableMetadata>,
+    pub upgradable_metadata: ProgramAccount<'info, UpgradableMetadata>,
 
     #[account(owner = spl_token::id())]
     pub mint: AccountInfo<'info>,
@@ -181,7 +254,7 @@ pub struct MintNFT<'info> {
 
     pub system_program: Program<'info, System>,
 
-    pub clock: AccountInfo<'info>
+    pub clock: Sysvar<'info, Clock>
 }
 
 #[derive(Accounts)]
@@ -212,7 +285,43 @@ pub struct UpgradeNFT<'info> {
     #[account(owner = spl_token::id())]
     pub token_program: AccountInfo<'info>,
 
-    pub clock: AccountInfo<'info>
+    pub clock: Sysvar<'info, Clock>
+}
+
+#[derive(Accounts)]
+#[instruction(nft_list_bump: u8)]
+pub struct StartRace<'info> {
+    #[account(mut, signer)]
+    pub operator: AccountInfo<'info>,
+
+    #[account(mut)]
+    pub race_result: ProgramAccount<'info, RaceResult>,
+
+    #[account(
+        mut,
+        seeds = [
+            (*program_id).as_ref(), 
+            "nft_list".as_ref()
+        ],
+        bump = nft_list_bump
+    )]
+    pub nft_list: AccountInfo<'info>,    
+    
+    #[account(
+        mut,
+        seeds = [
+            (*program_id).as_ref(), 
+            "operator_list".as_ref()
+        ],
+        bump = operator_list.bump
+    )]
+    pub operator_list: ProgramAccount<'info, OperatorWhiteList>,
+
+    pub sol_feed_account: AccountInfo<'info>,
+
+    pub btc_feed_account: AccountInfo<'info>,
+
+    pub clock: Sysvar<'info, Clock>
 }
 
 #[account]
@@ -223,8 +332,56 @@ pub struct UpgradableMetadata {
 }
 
 #[account]
-#[derive(Default)]
 pub struct OperatorWhiteList {
     pub operator_array: [Pubkey; 10],
     pub operator_cnt: u8,
+    pub bump: u8
+}
+
+#[account]
+pub struct RaceResult {
+    pub winner: Pubkey,
+}
+
+pub fn add_nft<'info> (
+    nft_mint: Pubkey,
+    nft_list: AccountInfo<'info>
+) -> ProgramResult {
+    let mut nft_list_data = nft_list.data.borrow_mut();
+    let count = get_nft_count(nft_list)?;
+    let start: usize = (2 + count * 32) as usize;
+    nft_mint.serialize(&mut &mut nft_list_data[start..start+32])?;
+    Ok(())
+}
+pub fn get_nft_count<'a> (
+    nft_list: AccountInfo<'a>
+) -> Result<u16, ProgramError> {
+    let mut nft_list_data = nft_list.data.borrow();
+    let count: u16 = u16::try_from_slice(&nft_list_data[0..2])?;
+    Ok(count)
+}
+
+pub fn increase_nft_count<'a> (
+    nft_list: AccountInfo<'a>
+) -> ProgramResult {
+    let mut nft_list_data = nft_list.data.borrow_mut();
+    let count: u16 = u16::try_from_slice(&nft_list_data[0..2])?;
+    count += 1;
+    let data = count.to_le_bytes();
+
+    for i in 0..2 {
+        nft_list_data[i] = data[i];
+    }
+
+    Ok(())
+}
+
+pub fn get_winner<'a> (
+    idx: u64,
+    nft_list: AccountInfo<'a>
+) -> Result<Pubkey, ProgramError>{
+    let mut nft_list_data = nft_list.data.borrow();
+    let start: usize = (2 + idx * 32) as usize;
+    let winner_pk: Pubkey = Pubkey::try_from_slice(&nft_list_data[start .. start + 32])?;
+    Ok(winner_pk)
 }
